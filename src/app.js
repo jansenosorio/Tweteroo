19961f3e25859619c5f9de5804fd9926a20fa392
Jansen Osorio <dev.jansen93@gmail.com>
2023-01-05 20:06:39 -0300

    feat: started server at my localhost

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ import cors from 'cors'
 
 const app = express()
 
-app.listen(5000)
+// Server configuration
 
+app.use(cors())
+app.listen(5000, () => {
+    console.log('Servidor iniciado!')
+})
+
+// Variables from my application
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
